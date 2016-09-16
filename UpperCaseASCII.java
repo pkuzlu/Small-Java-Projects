ef5a2096428c2e89a8af975d5d45e48dc97d8808
pkuzlu <pinarkuzlu@gmail.com>
2016-09-16 16:29:59 -0400

    Add files via upload

---
+++ UpperCaseASCII.java
@@ -0,0 +1,15 @@
+/**
+ * Created by pkuzlu on 9/16/2016.
+ */
+public class UpperCaseASCII {
+
+    public static void main(String[] args)
+    {
+        for(char c = 'A'; c<= 'Z'; c++)
+        {
+            System.out.println("Letter: " + c + " ASCII: " + (int)c);
+
+        }
+
+    }
+}
